31190cdb814dd96eeff4cf93b7bf5a49ed77cfae
WhitegoId <taleforhelen@gmail.com>
2019-08-22 08:18:35 +0300

    Edit Map #2

--- Source/TheBorder/TheBorderGameMode.cpp
+++ Source/TheBorder/TheBorderGameMode.cpp
@@ -7,7 +7,7 @@
 ATheBorderGameMode::ATheBorderGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_TheBorderCharacter.BP_TheBorderCharacter.BP_C"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
